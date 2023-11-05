8f14921f380c1b608a2d15a6b5da6e496d173947
Luiz Piano <44480258+luizpianooficial@users.noreply.github.com>
2023-11-05 15:38:35 -0300

    Update historico_N.dart

--- AppTelecon/lib/pages/RH/historico_N.dart
+++ AppTelecon/lib/pages/RH/historico_N.dart
@@ -26,7 +26,7 @@ class _HistoricoState extends State<Historico> {
 
   Future<void> carregarRegistros() async {
     var url = Uri.parse(
-        'https://sistema32.cloud/move/Api/VPS/RH/puxa_solic_tec.php?user=${widget.loggedInUser}');
+        'API');
     try {
       var response = await http.get(url);
 
